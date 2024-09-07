7c051d2db821942bec08796d53dd117f3afe60cd
Donald Hicks <s187777@students.cdu.edu.au>
2024-09-07 21:16:18 +0930

    question 3

---
+++ ASS2Q3 - Fix error prone codes.py
@@ -0,0 +1,15 @@
+"""
+Question 3
+
+Fixing the error-prone codes.
+
+Below is the code (left) that is encrypted using a number. 
+Once you decrypt the below code, it reveals the original code with many errors. 
+Please fix them and explain them using comments (#).
+
+To decrypt the above code, first, you need to understand how it is encrypted (above right image).
+1.  Fixing the next code will reveal the key.
+2.  Write the decryption function to decrypt the ‘encrypted code’ to the original code.
+3.  Correct the errors and provide the comments.
+4.  Should show everything in your program file.
+"""
