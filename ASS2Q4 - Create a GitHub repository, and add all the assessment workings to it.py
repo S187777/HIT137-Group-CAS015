0e605db5a1dc4e6bf19cfa4feb33039925bad3a5
Donald Hicks <s187777@students.cdu.edu.au>
2024-09-07 21:15:21 +0930

    question 4 - create a github repository, and use that to complete the assessment

---
+++ ASS2Q4 - Create a GitHub repository, and add all the assessment workings to it.py
@@ -0,0 +1,8 @@
+"""
+Question 4
+
+Welcome to the final task of this assignment. You are required to create a GitHub repository and 
+add all your group mates to it (make sure to keep it public, not private). 
+You should do this before you start the assignment.
+All the answers and contributions should be recorded in GitHub till you submit the assignment.
+"""
