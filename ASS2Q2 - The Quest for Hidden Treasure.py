60d4954167ad4680cdf1f9cd2d8c2814bc3cff77
Donald Hicks <s187777@students.cdu.edu.au>
2024-09-07 21:11:54 +0930

    question 2

---
+++ ASS2Q2 - The Quest for Hidden Treasure.py
@@ -0,0 +1,47 @@
+"""
+Question 2
+Here’s an adventurous story intertwined with Python programming questions that involve 
+nested 'for' loops, conditional statements, string manipulations, and more.
+
+The Quest for the Hidden Treasure:
+Deep within the mystical lands of Pythoria lay the fabled Temple of Codes, 
+rumored to house a treasure of knowledge guarded by enigmatic puzzles. 
+The path is challenging, and only those who can do the coding will unravel the final word, 
+leading to the treasure.
+
+Chapter 1: The Gatekeeper
+The above algorithm generates a number (n). 
+You should use this number to change the pixels (r,g,b) in the provided image (Chapter1.png) 
+by adding the original pixel values (r,g,b) with the generated number (Example: (r+n, g+n, b+n)).
+Generate a new image with the converted pixels (upload it as ‘chapter1out.png’).
+Finally, add all the red (r) pixel values in the new_image and provide the sum as output to move to the next chapter.
+
+Chapter 2: The Chamber of Strings
+Pt1.
+Assume s is a string.
+Write a program that separates a long string (at least length of 16)
+that contains both numbers and letters (upper and lower case)
+into two substrings of numbers and letters.
+Convert the even numbers in "number substring"
+and upper case letters in the "letter string"
+into ASCII code decimal values.
+
+Example Scenario:
+String = "56aAww1984sktr235270aYmn145ss785fsq31D0"
+Separate them: 561984235270145785310 (number string) and aAwwsktraYmnssfsqD (letter string)
+Convert the even numbers in the number string to ASCII code decimal values:
+6, 8, 4, 2, 2, 0, 4, 8, 0 (even numbers)
+becomes 54, 56, 50, 50, 48, 52, 56, 48 (ASCII decimal values)
+
+Convert the upper-case letters in the letter string to ASCII code decimal values:
+A, Y, D (upper case letters)
+becomes 65, 89, 68 (ASCII decimal values)
+
+Pt2.
+You are required to create a program that showcases the required output for the following question:
+Many newspapers publish a cryptogram each day, for instance:
+VZ FRYSVFU VZCNGVRAG NAQ N YVGGYR VAFRPHER V ZVFGNXRF V NZ BHG BS PBAGEBY
+NAQNG GVZRF UNEQ GB UNAQYR OHG VS LBH PNAG UNAQYR ZR NG ZL JBEFG GURA LBH FHER NF
+URYYQBAG QRFREIR ZR NG ZL ORFG ZNEVYLA ZBAEBR
+
+"""
