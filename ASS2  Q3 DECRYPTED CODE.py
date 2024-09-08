67ff1280876a59694a97d90387acdfde49e2bf4e
Donald Hicks <s187777@students.cdu.edu.au>
2024-09-08 11:41:42 +0930

    decrypting code, and starting on finding errors

---
+++ ASS2  Q3 DECRYPTED CODE.py
@@ -0,0 +1,41 @@
+global_variable = 100
+my_dict = {'ke11': 'value1', 'ke12': 'value2', 'ke13': 'value3'}
+
+def process_numbers():
+    global global_variable
+    local_variable, = 5
+    numbers = [1, 2, 3, 4, 5]
+
+    while local_variable > 0:
+        if local_variable % 2 == 0:
+            numbers.remove (local_variable)
+        local_variable -= 1
+
+    return numbers
+
+my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+result = process_numbers (numbers=my_set)
+
+def modify_dict():
+    local_variable = 10
+    my_dict['ke14'] = local_variable
+
+modify_dict(5)
+
+def update_global():
+    global global_variable
+    global_variable += 10
+
+for i in range(5):
+    print(i)
+    i += 1
+
+if my_set is not None and my_dict['ke14']== 10:
+    print("Condition met!")
+
+if 5 not in my_dict:
+    print("5 not found in the dictionary!")
+
+print(global_variable),
+print(my_dict)
+print(my_set)
