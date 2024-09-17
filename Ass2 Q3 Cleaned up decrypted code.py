159d2beb28cb172698b30a7450b020243f5448dc
Donald Hicks <s187777@students.cdu.edu.au>
2024-09-17 19:16:07 +0930

    file to show the cleaned up decrypted code and fixes

---
+++ Ass2 Q3 Cleaned up decrypted code.py
@@ -0,0 +1,155 @@
+    #   Decrypted OUTPUT to Fix     #
+
+global_variable = 100       # Define a variable "global_variable"
+
+my_dict = {'ke11': 'value1', 'ke12': 'value2', 'ke13': 'value3'}        # Define a dictionary
+
+
+"""
+def process_numbers():         # Define a function with no argument.
+    global global_variable          # Use global to modify a local variable inside a function.
+    local_variable = 5          # Assign a local variable
+    numbers = [1, 2, 3, 4, 5]       # Assign another local Variable
+
+    while local_variable > 0:           # While loop to remove even numbers from a list or set.
+        if local_variable % 2 == 0:
+            numbers.remove(local_variable)
+        local_variable -= 1
+
+    return numbers      # Return output.
+
+
+Discussion notes:
+The intent of this function seems to be to remove any even numbers from a list (or set).
+    If so, then the function requires an argument, i.e. a list, or a set.
+The global_variable is assigned global, but not modified in this function.
+    So we can remove it.
+The numbers list is a variable local to this function.
+    So we can make it global or move it outside of the function.
+The *.remove() function removes the first occurance of an element in a list.
+    Do we need to add a check for multiple numbers in list??
+The *.remove() function will raise a ValueError if the value is not in the list.
+    We need to add an error exception case.
+The variable local_variable is 5, but is this to ensure the while loop iterates through the whole list?
+    If so, then we need to make local_variable = len(list).
+The return function returns a new list with the values removed.
+
+The cleaned up code would then read:
+"""
+
+def process_numbers(numbers):
+    local_variable = len(numbers)
+
+    while local_variable > 0:
+        if local_variable % 2 == 0:
+            numbers.remove(local_variable)
+        local_variable -= 1
+
+    return numbers
+
+numbers = [1, 2, 3, 4, 5]
+
+process_numbers(numbers)
+
+# Output: [1, 3, 5]
+
+
+my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}         
+# A set of integers including duplicates.
+# Duplicates will be ignored when the set is called.
+
+"""
+result = process_numbers(numbers=my_set)
+
+Discussion notes:
+Calling the function above and assigning the returned list to a variable.
+The original function did not require any arguments, but that has changed.
+The argument 'numbers = my_set' indicates the code is intended to iterate through the set.
+    We can just remove 'numbers =', or '= my_set'.
+"""
+# Clean code:
+result = process_numbers(numbers)
+
+# If we wanted to run this function on the my_set set:
+
+numbers = my_set
+result = process_numbers(numbers)
+
+"""
+def modify_dict():          # A function to add a value (10) to the key 'ke14'.
+                            # This function does not expect an argument.
+    local_variable = 10
+    my_dict['ke14'] = local_variable
+
+modify_dict(5)
+
+The intent of this function seems to be to add a value, or a key/value pair to a dictionary.
+    If the intent is to add a key/value pair, then we will require two arguments in the function.
+    i.e. modify_dict(key, value):
+This would then oblige us to change the code to this:
+"""
+def modify_dict(key, value):
+    local_variable = value
+    my_dict[key] = local_variable
+
+modify_dict('ke14', 5)
+
+"""
+Otherwise, if the intent is to just add the value 10 to the key 'ke14', 
+We would remove all arguments in the function, and the call.
+
+We could then change the code to this:
+"""
+def modify_dict():
+    local_variable = 10
+    my_dict['ke14'] = local_variable
+
+modify_dict()
+
+"""
+def update_global():
+    global global_variable
+    global_variable += 10
+# This funtion appears to call the global variable 'global_variable' assigned at the begining of this decrypted code,
+# Then proceeds to add 10 to the variable.
+# The function is never called, however.
+
+If the intent is to update global_variable with a value, we can add an argument, and call the function.
+"""
+def update_global(value):
+    global global_variable
+    global_variable += value
+
+update_global(10)
+"""
+Else, If the intent is to just change the global_variable by 10, every time it is called,
+we can simply call it by using:
+
+def update_global():
+    global global_variable
+    global_variable += 10
+update_global()
+
+However, this seems a very large piece of code for a limited use.
+"""
+
+
+"""
+for i in range(5):
+    print(i)
+    i += 1
+
+Discussion:
+
+"""
+
+
+if my_set is not None and my_dict['ke14'] == 10:
+    print("Condition met!")
+
+if 5 not in my_dict:
+    print("5 not found in the dictionary!")
+
+print(global_variable)
+print(my_dict)
+print(my_set)
